13d2d527a41dd8cc18592f10ffef286573c58d21
Saad Mehmood <saad.mehmood@viltco.com>
2022-03-27 00:29:28 +0500

    Post

--- src/HomePage/footer.js
+++ src/HomePage/footer.js
@@ -9,27 +9,40 @@ import "./footerTwo.css";
 const Footer = () => {
   return (
     <>
-      <div className="footer col-sm-12  " >
+      <div className="footer footer-bg col-sm-12  ">
         <div className="col-md-4 image-left">
           <img src={FooterLeft} alt="" className="img-fluid" />
         </div>
-    
-          <div className="col-md-4 footer-content-style  ">
-            <h3>ROXO</h3>
-      <p>Copyrights 2022 ROXO LTD.
-        <br/>
-        All rights reserved.
-      </p>
-      <p>Contact US | Careers | White Paper</p>
-           
-          </div>
-        
+
+        <div className="col-md-4 footer-content-style  ">
+          <h3>ROXO</h3>
+          <p>
+            Copyrights 2022 ROXO LTD.
+            <br />
+            All rights reserved.
+          </p>
+          <p>Contact US | Careers | White Paper</p>
+        </div>
+
         <div className="col-md-4 ">
           <div className="col-sm-12 text-right image-right">
             <img src={FooterRight} alt="" className="img-fluid" />
           </div>
         </div>
         {/* </div> */}
+      </div>
+
+      {/* -----------resp footer-------- */}
+      <div className="footer-resp">
+        <div className="col-md-4 footer-content-style   ">
+          <h3>ROXO</h3>
+          <p>
+            Copyrights 2022 ROXO LTD.
+            <br />
+            All rights reserved.
+          </p>
+          <p>Contact US | Careers | White Paper</p>
+        </div>
       </div>
     </>
   );
